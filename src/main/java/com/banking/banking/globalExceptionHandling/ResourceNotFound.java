8f424f1b48fcf1c1b278f8b2f295c3b70189eeb1
Akshay Ayush <akshayayush2610@gmail.com>
2024-04-27 13:58:14 +0530

    resource not found exception

---
+++ src/main/java/com/banking/banking/globalExceptionHandling/ResourceNotFound.java
@@ -0,0 +1,7 @@
+package com.banking.banking.globalExceptionHandling;
+
+public class ResourceNotFound extends RuntimeException {
+    public ResourceNotFound(String message) {
+        super(message);
+    }
+}
